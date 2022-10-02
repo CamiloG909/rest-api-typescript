4cd3c14641d83e54412c28797fddfe45cccb9606
Camilo Galindo R <cagalindo35@misena.edu.co>
2022-10-01 22:02:53 -0500

    fix host

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import app from './server';
 
-app.listen(4000, () => console.log('Server is running on port 4000'));
+app.listen(4000, '0.0.0.0', () => console.log('Server is running on port 4000'));
